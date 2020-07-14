34d0cb33cb1b4f9531f04027594644bb20fa6585
SKY <sunil7545@gmail.com>
2020-07-14 16:58:31 +0200

    formatting

--- ocr_test_2.py
+++ ocr_test_2.py
@@ -1,28 +1,34 @@
 import os
 import boto3
-# os.environ['AWS_DEFAULT_REGION'] = 'eu-central-1'
+
+
+def extract_text(file_name):
+
+    # Read document content
+    with open(file_name, 'rb') as document:
+        image_bytes = bytearray(document.read())
+
+    # Amazon Textract client
+    text_extract = boto3.client('textract', region_name='eu-west-1')
+
+    # Call Amazon Text-ract
+    response = text_extract.detect_document_text(Document={'Bytes': image_bytes})
+
+    detected_lines = []
+    # Print detected text
+    for item in response["Blocks"]:
+        if item["BlockType"] == "LINE":
+            loc_variable = '\033[94m' + item["Text"] + '\033[0m'
+            # print(loc_variable)
+            detected_lines.append(loc_variable)
+
+    print(detected_lines)
+    return detected_lines
+
 
 # Document
-documentName = "a.jpg"
+document_name = "a.jpg"
+text_extracted = extract_text(document_name)
 
-# Read document content
-with open(documentName, 'rb') as document:
-    imageBytes = bytearray(document.read())
 
-# Amazon Textract client
-textract = boto3.client('textract', region_name='eu-west-1')
 
-# Call Amazon Textract
-response = textract.detect_document_text(Document={'Bytes': imageBytes})
-
-#print(response)
-
-detected_lines = []
-# Print detected text
-for item in response["Blocks"]:
-    if item["BlockType"] == "LINE":
-        loc_variable = '\033[94m' + item["Text"] + '\033[0m'
-        print(loc_variable)
-        detected_lines.append(loc_variable)
-        
-print(len(detected_lines))
