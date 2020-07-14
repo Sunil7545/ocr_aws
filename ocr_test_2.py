c943e25734bd67a395f17b4e4b674c84a2d519ae
SKY <sunil7545@gmail.com>
2020-07-14 17:03:01 +0200

    string conversion

--- ocr_test_2.py
+++ ocr_test_2.py
@@ -19,7 +19,7 @@ def extract_text(file_name):
     for item in response["Blocks"]:
         if item["BlockType"] == "LINE":
             # loc_variable = '\033[94m' + item["Text"] + '\033[0m'
-            loc_variable = item["Text"]
+            loc_variable = str(item["Text"])
             # print(loc_variable)
             detected_lines.append(loc_variable)
 
