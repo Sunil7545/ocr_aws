67d00e72626213a97eaf97a8c696cb319bba1fde
SKY <sunil7545@gmail.com>
2021-01-02 15:38:28 +0100

    restructured and removed jpg

--- ocr_text.py
+++ ocr_text.py
@@ -5,16 +5,10 @@ import argparse
 import tempfile
 import numpy as np
 import matplotlib.image as mpimg
+import os.path as op
+
 
 def extract_text(file_name):
-
-    # configure S3
-
-    # s3 = boto3.resource('s3', region_name='eu-west-1')
-    # bucket = s3.Bucket(file_name)
-    # object = bucket.Object('a.jpg')
-    # tmp = tempfile.NamedTemporaryFile()
-    # print(tmp.name)
     # Read document content
     with open(file_name, 'rb') as document:
         image_bytes = bytearray(document.read())
@@ -40,27 +34,35 @@ def extract_text(file_name):
     return detected_lines
 
 
-# Document
-# document_name = "a.jpg"
-# input folder name, which has data
-parser = argparse.ArgumentParser()
-parser.add_argument("image", help="select the input image for text extraction", type=str)
-parser.add_argument("key_word", help="to check in the document", type=str)
-args = parser.parse_args()
-document_name = args.image
-key_name = args.key_word
-text_extracted = extract_text(document_name)
-s3 = boto3.resource('s3', region_name='eu-west-1')
-# s3 = boto3.client('s3')
-bucket = s3.Bucket('textract-data-sky')
-object = bucket.Object('a.jpg')
-tmp = tempfile.NamedTemporaryFile()
-with open(tmp.name, 'wb') as f:
-    object.download_fileobj(f)
-    img = mpimg.imread(tmp.name)
+def get_files_s3(bucket_name):
+    s3 = boto3.resource('s3', region_name='eu-west-1')
+
+    bucket = s3.Bucket(bucket_name)
+    object = bucket.Object('a.jpg')
+    object.download_file('a.jpg')
+    b_flag = op.isfile('a.jpg') 
+    # tmp = tempfile.NamedTemporaryFile()
+    # with open(tmp.name, 'wb') as f:
+    #    object.download_fileobj(f)
+    #    img = mpimg.imread(tmp.name)
     # img = bytearray(f.read())
-    print(np.array(img).shape)
-print(text_extracted.count(key_name))
+    print("file is downloaded: " + str(b_flag))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image", help="select the input image for text extraction", type=str)
+    parser.add_argument("key_word", help="to check in the document", type=str)
+    args = parser.parse_args()
+    document_name = args.image
+    key_name = args.key_word
+    text_extracted = extract_text(document_name)
+
+    print(text_extracted.count(key_name))
+
+
+if __name__ == '__main__':
+    get_files_s3('textract-data-sky')
+    
 
 
 
